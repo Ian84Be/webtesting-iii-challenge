578453ab96060d17bff6d2eddce1ab88a327e54b
Ian Be <ian84be@gmail.com>
2019-04-10 17:56:16 -0500

    testing for red/green led class when open/closed

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -2,30 +2,22 @@
 import React from 'react';
 import { render, fireEvent } from 'react-testing-library';
 import renderer from 'react-test-renderer';
+import 'jest-dom/extend-expect';
 import 'react-testing-library/cleanup-after-each'; 
 
 import Display from './Display';
 
 describe('Display', () => {
 
-    describe('CLOSED STATE', () => {
-        it('displays Closed if the closed prop is true', () => {
-            const {getByText} = render(<Display closed={true}/>);
-            expect(getByText(/closed/i));
-        });
-    });
-
     describe('OPEN STATE', () => {
         it('displays Open if the closed prop is false', () => {
             const {getByText} = render(<Display closed={false}/>);
             expect(getByText(/open/i));
         });
-    });
 
-    describe('LOCKED STATE', () => {
-        it('displays Locked if the locked prop is true', () => {
-            const {getByText} = render(<Display locked={true}/>);
-            expect(getByText(/locked/i));
+        it('uses green-led class when open', () => {
+            const {getByText} = render(<Display closed={false}/>);
+            expect(getByText(/open/i)).toHaveClass('green-led');
         });
     });
 
@@ -34,5 +26,35 @@ describe('Display', () => {
             const {getByText} = render(<Display locked={false}/>);
             expect(getByText(/unlocked/i));
         });
+
+        it('uses green-led class when unlocked', () => {
+            const {getByText} = render(<Display locked={false}/>);
+            expect(getByText(/open/i)).toHaveClass('green-led');
+        });
     });
+
+    describe('CLOSED STATE', () => {
+        it('displays Closed if the closed prop is true', () => {
+            const {getByText} = render(<Display closed={true}/>);
+            expect(getByText(/closed/i));
+        });
+
+        it('uses red-led class when closed', () => {
+            const {getByText} = render(<Display closed={true}/>);
+            expect(getByText(/closed/i)).toHaveClass('red-led');
+        });
+    });
+
+    describe('LOCKED STATE', () => {
+        it('displays Locked if the locked prop is true', () => {
+            const {getByText} = render(<Display locked={true}/>);
+            expect(getByText(/locked/i));
+        });
+
+        it('uses red-led class when locked', () => {
+            const {getByText} = render(<Display locked={true}/>);
+            expect(getByText(/locked/i)).toHaveClass('red-led');
+        });
+    });
+
 });
