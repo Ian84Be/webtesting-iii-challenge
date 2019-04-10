76cc2149fb95b412d4a39211b98f30ee8d124ca2
Ian Be <ian84be@gmail.com>
2019-04-10 16:02:32 -0500

    test for default unlocked&open

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,16 @@
-// Test away
+
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import renderer from 'react-test-renderer';
+import 'react-testing-library/cleanup-after-each'; 
+
+import Dashboard from './Dashboard';
+
+describe('Dashboard', () => {
+    it('should default to unlocked & open', () => {
+        const {getByText} = render(<Dashboard/>);
+        expect(getByText(/unlocked/i));
+        expect(getByText(/open/i));
+
+    });
+});
