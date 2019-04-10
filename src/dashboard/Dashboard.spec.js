e505c8c11d6da49260f6933b33f87f4c5621f531
Ian Be <ian84be@gmail.com>
2019-04-10 17:07:27 -0500

    test Display component

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -7,28 +7,45 @@ import 'react-testing-library/cleanup-after-each';
 import Dashboard from './Dashboard';
 
 describe('Dashboard', () => {
-    it('defaults to unlocked & open', () => {
-        const {getByText} = render(<Dashboard/>);
-        expect(getByText(/unlocked/i));
-        expect(getByText(/open/i));
+    describe('DEFAULT STATE', () => {
+        it('matches snapshot', () => {
+            const tree = renderer.create(<Dashboard />).toJSON();
+            expect(tree).toMatchSnapshot();
+        });
+
+        it('defaults to unlocked & open', () => {
+            const {getByText} = render(<Dashboard/>);
+            expect(getByText(/unlocked/i));
+            expect(getByText(/open/i));
+        });
+
+        it('shows the controls and display', () => {
+            const {getByText} = render(<Dashboard/>);
+            expect(getByText(/unlocked/i));
+            expect(getByText(/open/i));
+            expect(getByText(/close gate/i));
+            expect(getByText(/lock gate/i));
+        });
     });
 
-    it('cannot be closed or opened if it is locked', () => {
-        const {getByText} = render(<Dashboard/>);
-        
-        expect(getByText(/unlocked/i));
-        expect(getByText(/open/i));
-
-        fireEvent.click(getByText(/close gate/i));
-        expect(getByText(/closed/i));
-        expect(getByText(/unlocked/i));
-
-        fireEvent.click(getByText(/lock gate/i));
-        expect(getByText(/closed/i));
-        expect(getByText(/locked/i));
-
-        fireEvent.click(getByText(/open gate/i));
-        expect(getByText(/closed/i));
-        expect(getByText(/open gate/i));
+    describe('LOCKED STATE', () => {
+        it('cannot be closed or opened if it is locked', () => {
+            const {getByText} = render(<Dashboard/>);
+            
+            expect(getByText(/unlocked/i));
+            expect(getByText(/open/i));
+    
+            fireEvent.click(getByText(/close gate/i));
+            expect(getByText(/closed/i));
+            expect(getByText(/unlocked/i));
+    
+            fireEvent.click(getByText(/lock gate/i));
+            expect(getByText(/closed/i));
+            expect(getByText(/locked/i));
+    
+            fireEvent.click(getByText(/open gate/i));
+            expect(getByText(/closed/i));
+            expect(getByText(/open gate/i));
+        });
     });
 });
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,38 @@
-// Test away!
+
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import renderer from 'react-test-renderer';
+import 'react-testing-library/cleanup-after-each'; 
+
+import Display from './Display';
+
+describe('Display', () => {
+
+    describe('CLOSED STATE', () => {
+        it('displays Closed if the closed prop is true', () => {
+            const {getByText} = render(<Display closed={true}/>);
+            expect(getByText(/closed/i));
+        });
+    });
+
+    describe('OPEN STATE', () => {
+        it('displays Open if the closed prop is false', () => {
+            const {getByText} = render(<Display closed={false}/>);
+            expect(getByText(/open/i));
+        });
+    });
+
+    describe('LOCKED STATE', () => {
+        it('displays Locked if the locked prop is true', () => {
+            const {getByText} = render(<Display locked={true}/>);
+            expect(getByText(/locked/i));
+        });
+    });
+
+    describe('UNLOCKED STATE', () => {
+        it('displays Unlocked if the locked prop is false', () => {
+            const {getByText} = render(<Display locked={false}/>);
+            expect(getByText(/unlocked/i));
+        });
+    });
+});
